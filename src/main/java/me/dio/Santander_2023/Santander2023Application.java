4617d16d08c1bf887863246aed865248927846e5
jeferson <jeferson9488@gmail.com>
2024-07-23 23:02:16 -0300

    Initial commit

---
+++ src/main/java/me/dio/Santander_2023/Santander2023Application.java
@@ -0,0 +1,13 @@
+package me.dio.Santander_2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Santander2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Santander2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/Santander_2023/Santander2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.Santander_2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Santander2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
